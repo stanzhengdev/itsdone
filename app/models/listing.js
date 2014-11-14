b67b3cb0dab93eb4f0830461c8a7e1e8afcd9ac9
stanley zheng <dev.stanzheng@gmail.com>
2014-11-13 21:55:56 -0800

    updating relations

--- app/models/listing.js
+++ app/models/listing.js
@@ -10,9 +10,24 @@ var listingSchema = mongoose.Schema({
   provider: { type: String },
   subscribers: {type: Array}, //, unique: true?
   closed: {type: Boolean, default: false},
-  modified: { type: Date, default: Date.now }
+  modified: { type: Date, default: Date.now },
+  created: { type: Date, default: Date.now },
+  tags: {type: Array},
+  reviews: {type: Array}
 });
 
 
 // create the model for users and expose it to our app
 module.exports = mongoose.model('listing', listingSchema);
+
+/*
+//reviewsschema
+
+{
+  date: { type: Date, default: Date.now },
+  review: { type: String, required: true,  unique: false  },
+  title: { type: String, required: true,  unique: false  },
+  rating: { type: Number }
+
+}
+*/
